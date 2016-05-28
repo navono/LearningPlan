bdaec38528fffc859d3da6153605d895fc92b17d
navono007 <navono007@gmail.com>
2016-05-28 22:11:54 +0800

    GeoQuiz工程修改，已可以运行

--- 2016/Android/_code/GeoQuiz/app/src/main/java/com/example/navono/geoquiz/GeoQuiz.java
+++ 2016/Android/_code/GeoQuiz/app/src/main/java/com/example/navono/geoquiz/GeoQuiz.java
@@ -1,5 +1,6 @@
 package com.example.navono.geoquiz;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 public class GeoQuiz extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +24,25 @@ public class GeoQuiz extends AppCompatActivity {
         setContentView(R.layout.activity_geo_quiz);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(GeoQuiz.this,
+                        R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(GeoQuiz.this,
+                        R.string.correct_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
