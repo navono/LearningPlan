d1bd77b2c27385015dc9edff4ad927b971aa2cd0
navono007 <navono007@gmail.com>
2016-06-09 07:01:35 +0800

    增加了一些Log，用来查看Activity的生命周期

--- 2016/Android/_code/GeoQuiz/app/src/main/java/com/example/navono/geoquiz/GeoQuiz.java
+++ 2016/Android/_code/GeoQuiz/app/src/main/java/com/example/navono/geoquiz/GeoQuiz.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -35,6 +36,9 @@ public class GeoQuiz extends AppCompatActivity {
     };
 
     private int mCurrentIndex = 0;
+
+    private static final String TAG = "GeoQuiz";
+
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -44,7 +48,10 @@ public class GeoQuiz extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
+
         setContentView(R.layout.activity_geo_quiz);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -134,6 +141,7 @@ public class GeoQuiz extends AppCompatActivity {
     @Override
     public void onStart() {
         super.onStart();
+        Log.d(TAG, "onStart() called");
 
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -154,6 +162,7 @@ public class GeoQuiz extends AppCompatActivity {
     @Override
     public void onStop() {
         super.onStop();
+        Log.d(TAG, "onStop() called");
 
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -170,4 +179,23 @@ public class GeoQuiz extends AppCompatActivity {
         AppIndex.AppIndexApi.end(mClient, viewAction);
         mClient.disconnect();
     }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
 }
