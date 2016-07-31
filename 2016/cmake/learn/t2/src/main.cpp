f64a0db0801e6df444defc2631b0378292b28876
navono007 <navono007@gmail.com>
2016-07-31 20:59:48 +0800

    cmake学习资料

---
+++ 2016/cmake/learn/t2/src/main.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello Word from t1.\n");
+	return 0;
+}
---
+++ 2016/cmake/learn/t3/lib/hello.h
@@ -0,0 +1,7 @@
+#ifndef HELLO_H_
+#define HELLO_H_
+#include <stdio.h>
+
+void HelloFunc();
+
+#endif
